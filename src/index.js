1bea27f88b4fc2d1ce9559df7723e4d228e5149d
jeevanK <jeevankumark@zen3.com>
2024-07-16 18:28:49 +0530

    Deploy my first lambda

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-export const handler = async (event) => {
-    // TODO implement
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify('Hello from Lambda!-From Github'),
-    };
-    return response;
+'use strict';
+
+module.exports.hello = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
   };
-  
+};
